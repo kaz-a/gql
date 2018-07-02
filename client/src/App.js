63998f5e0be0bf9a8692c7f4d11be655e3cb94c5
KAZUE ANAN <zakscloset@gmail.com>
2018-07-02 00:06:09 -0400

    booklist component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'; // ingect data from server into react app
+
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql' // request to this endpoint
+})
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>My Reading List</h1>
-        <BookList />
-      </div>
+      <ApolloProvider client={ client }>
+        <div id="main">
+          <h1>My Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
+import { gql } from 'apollo-boost'; // parse graphql queries
+import { graphql } from 'react-apollo'; // bind react to apollo
+
+// queries template string
+const getBooksQuery = gql`
+  {
+    books{
+      name
+      id
+    }
+  }
+`
 
 class BookList extends Component {
+  displayBooks(){
+    var data = this.props.data;
+    if(data.loading){
+      return (
+        <div>Loading books...</div>
+      )
+    } else {
+      return data.books.map(book => {
+        return (
+          <li key={ book.id }>{ book.name }</li>
+        )
+      })
+    }
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div>
         <ul id="book-list">
-        <li>Book name</li>
+          { this.displayBooks() }
         </ul>
       </div>
     );
   }
 }
 
-export default BookList;
+// take getBooksQuery and bind it to the BookList component
+// we have acess to all the data that comes back from the query
+// and stored in the component props
+export default graphql(getBooksQuery)(BookList);
