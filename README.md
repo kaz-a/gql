ae89db3f1e4e34f9c7cc5215a98f0e4bb8f316b6
KAZUE ANAN <zakscloset@gmail.com>
2018-07-09 22:08:51 -0400

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+## GraphQL App
+Based on [this tutorial](https://www.youtube.com/channel/UCW5YeuERMmlnqo4oq8vwUpg?sub_confirmation=1)
+
+### Dev:
+1. `npm install`
+2. log into [mLab](https://mlab.com/login/?r=%2Fdatabases)
+3. create new database in mongoDB if necessary.
+
+4. Server (Start the GraphQL Server):
+* `cd server`
+* `nodemon app`
+
+5. Client (Start create-react-app):
+* `npm start`
+
--- server/app.js
+++ server/app.js
@@ -4,7 +4,14 @@ const express = require('express'),
 	graphqlHTTP = require('express-graphql'),
 	schema = require('./schema/schema'),
 	mongoose = require('mongoose'),
-	port = 4000 || process.env.PORT;
+  port = 4000 || process.env.PORT;
+
+// in this project, we're running requests on the server-side and client-side
+// so we must allow cross-origin requests 
+const cors = require('cors'); 
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mlab database
 mongoose.connect("mongodb://kaz:1a2b3c4d@ds263710.mlab.com:63710/zakscloset");
